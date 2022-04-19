83e7006f0b178d4c617615e595d6da55b28f3689
가은 <user@192.168.0.10>
2022-04-19 15:57:23 +0900

    2022-04-19 반복문2

--- src/com/javaex/ex04/Ex13.java
+++ src/com/javaex/ex04/Ex13.java
@@ -1,9 +1,40 @@
 package com.javaex.ex04;
 
+import java.util.Scanner;
+
 public class Ex13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요[0이면 종료]");
+		
+		int sum = 0; //합계
+		int num;
+		
+		/*
+		do {
+			num = sc.nextInt(); //입력값
+			sum = sum+num;
+			System.out.print("합계: "+sum);
+		}while(num != 0);
+		*/
+		
+		System.out.println("================");
+		
+		while(true) {
+			num = sc.nextInt(); //입력값
+			sum = sum+num;
+			System.out.println("합계: "+sum);
+			if(num==0) {
+				break;
+			}
+		}
+		
+		System.out.println("종료.");
+		
+		sc.close();
 
 	}
 
